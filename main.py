3ee305c8ea19daa6e577f09533ba3e2c367b151e
Lance Ellis <scdelance@gmail.com>
2020-08-15 00:25:37 -0500

    asd

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-import gpsd, subprocess
+import gpsd, subprocess, sys, getopt
 from subprocess import PIPE
 
+def main(argv):
+    opts, args = getopt.getopt(argv,"d")
 
-gpsd.connect(host="0.0.0.0", port=2947)
-gpsd.GpsResponse.mode = 3
+    gpsd.connect(host="0.0.0.0", port=2947)
+    gpsd.GpsResponse.mode = 3
 
-gps = gpsd.get_current()
-gps.mode = 3
+    gps = gpsd.get_current()
+    gps.mode = 3
 
-print(gpsd.device())
-print(gpsd.get_current())
-print(gps.altitude)
+    print(gpsd.device())
+    print(gpsd.get_current())
+    print(gps.altitude)
 
-uhub = subprocess.run(["/home/pi/gpsscript/uhubctl -a 0 -p 10"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    uhub = subprocess.run(["/home/pi/gpsscript/uhubctl -a 0 -p 10"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-print("Error is ", uhub.stderr)
-print("stdout is ", uhub.stdout)
+    if uhub.stderr == '':
+        print("Encoutered error at uhubctl: ", uhub.stderr)
+    else:
+        if opts[0] in ("-d"):
+            print(uhub.stdout)
+        print("I function correctly")
 
 
 
