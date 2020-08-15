9a5a7d4f0077df581ab071c1040cf2893a34c681
Lance Ellis <scdelance@gmail.com>
2020-08-14 23:41:22 -0500

    c

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import gpsd
+import gpsd, subprocess
 
 
 gpsd.connect(host="0.0.0.0", port=2947)
 gpsd.GpsResponse.mode = 3
-gps = gpsd.GpsResponse()
+
+gps = gpsd.get_current()
 gps.mode = 3
 
 print(gpsd.device())
 print(gpsd.get_current())
-gpsd.GpsResponse.altitude(gps)
 print(gps.altitude)
 
-
+subprocess.run(["/home/pi/gpsscript/uhubctl -a 0 -p 10"], shell=True)
 
 
 
