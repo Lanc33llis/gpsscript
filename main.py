185e06453c99378b3a68c938e636ae7d492806ee
Lance Ellis <scdelance@gmail.com>
2020-08-14 19:28:45 -0500

    c3

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import gpsd
 
-gpsd.connect(host="0.0.0.0", port=2947)
-
-gps = gpsd()
+gps = gpsd
 gps.connect(host="0.0.0.0", port=2947)
 gps.GpsResponse.mode = 0
 print(gps.device())
-print(gps.getcurrent())
+print(gps.get_current())
 
 
 
