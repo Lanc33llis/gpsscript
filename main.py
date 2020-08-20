79d204a9642deb189c3ff825ab63922f3fd57409
Lance Ellis <scdelance@gmail.com>
2020-08-20 12:41:09 -0500

    asdasaasd

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def main(argv):
         print("Lat " + str(float(lat)) + " Lon " + str(float(lon)))
         updateDirewolfConf()
         try:
-            subprocess.run(["sudo rtl_fm -f 144.39M - | direwolf -c direwolf.conf -r 24000 -D 1 -"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            subprocess.run(["sudo rtl_fm -f 144.39M - | direwolf -c direwolf.conf -r 24000 -D 1 -"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout= 60)
         except subprocess.TimeoutExpired:
             print("reset")
             continue
