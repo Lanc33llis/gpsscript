2fff8806d144289128946d6d95631eb160ca601e
Lance Ellis <scdelance@gmail.com>
2020-08-20 10:53:45 -0500

    adas

--- main.py
+++ main.py
@@ -60,10 +60,10 @@ def main(argv):
 
     while True:
         lat, lon = gpsCheck()
-        updateDirewolfConf()
-        aprs = subprocess.run(["sudo rtl_fm -f 144.39M - | direwolf -c direwolf.conf -r 24000 -D 1 -"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=60)
         print("Lat " + str(float(lat)) + " Lon " + str(float(lon)))
-        time.sleep(61)
+        updateDirewolfConf()
+        aprs = subprocess.run(["sudo rtl_fm -f 144.39M - | direwolf -c direwolf.conf -r 24000 -D 1 -"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=120)
+        time.sleep(121)
 
     
     for opt, arg in opts:
