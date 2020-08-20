5cd59a4f7680c4d77b8e387cb298fca34edcd44b
Lance Ellis <scdelance@gmail.com>
2020-08-20 10:48:40 -0500

    asdsa

--- main.py
+++ main.py
@@ -59,6 +59,7 @@ def main(argv):
     # else:
 
     while True:
+        lat, lon = gpsCheck()
         updateDirewolfConf()
         aprs = subprocess.run(["sudo rtl_fm -f 144.39M - | direwolf -c direwolf.conf -r 24000 -D 1 -"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=60)
         print("Lat " + str(float(lat)) + " Lon " + str(float(lon)))
